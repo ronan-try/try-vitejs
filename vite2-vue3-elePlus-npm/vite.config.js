8e60f0c28ec5979a0903f611736b9846d23b55c5
Ronan <liruonan@hotmail.com>
2021-04-06 21:31:28 +0800

    feat: vitejs 拆包

--- vite2-vue3-elePlus-npm/vite.config.js
+++ vite2-vue3-elePlus-npm/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 import visualizer from 'rollup-plugin-visualizer'
 import analyze from 'rollup-plugin-analyzer'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +12,14 @@ export default defineConfig({
     // analyze(),
     visualizer()
   ],
-  // rollupOptions: {
-  //   plugins: [
-  //     analyze(),
-  //     visualizer()
-  //   ],
-  // },
+  // build: {
+  //   rollupOptions: {
+  //     plugins: [ nodeResolve() ],
+  //     output: {
+  //       manualChunks: {
+  //         'ElementPlus': ['element-plus']
+  //       }
+  //     }
+  //   }
+  // }
 })
